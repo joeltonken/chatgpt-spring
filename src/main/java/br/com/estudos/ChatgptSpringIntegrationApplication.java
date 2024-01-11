941b4482a337d1b4c1cd0d5a7c21dc6e00de3761
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-01-11 10:45:14 -0300

    iniciando projeto

---
+++ src/main/java/br/com/estudos/ChatgptSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptSpringIntegrationApplication.class, args);
+	}
+
+}
