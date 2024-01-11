678a1c62a0247a8b3b90c083b22f0bc49ac8bf23
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-01-11 20:32:44 -0300

    add service

---
+++ src/main/java/br/com/estudos/service/ChatGPTService.java
@@ -0,0 +1,39 @@
+package br.com.estudos.service;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.estudos.vo.request.ChatGptRequest;
+import br.com.estudos.vo.response.ChatGptResponse;
+
+@Service
+public class ChatGPTService {
+
+	private Logger logger = Logger.getLogger(ChatGPTService.class.getName());
+    
+    @Value("${openai.model}")
+    private String model;
+    
+    @Value("${openai.api.url}")
+    private String url;
+    
+    @Autowired
+    private RestTemplate template;
+    
+    public String chat(String prompt) { 
+        
+        logger.info("Inciando Prompt");
+        
+        ChatGptRequest request = new ChatGptRequest(model, prompt);
+
+        logger.info("Processando Prompt");
+        ChatGptResponse response = 
+                template.postForObject(url, request, ChatGptResponse.class);
+
+        return response.getChoices().get(0).getMessage().getContent();
+    }
+}
